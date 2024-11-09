d4a26f2d965a28f3c2c23446ace9ef41ea10e2dd
mu7ammad1 <153460583+mu7ammad1@users.noreply.github.com>
2024-11-09 20:06:48 +0200

    1

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -27,7 +27,7 @@ export default function Error() {
           Uh-oh!
         </h1>
 
-        <p className="mt-4 text-gray-500">We can't find that page.</p>
+        <p className="mt-4 text-gray-500">We cant find that page.</p>
       </div>
     </div>
   );
