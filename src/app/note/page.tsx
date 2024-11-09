294526bc902c6a01b24e18745795fe677a358311
mu7ammad1 <153460583+mu7ammad1@users.noreply.github.com>
2024-11-09 20:08:59 +0200

    1

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -1,16 +1,9 @@
 import { sql } from "@vercel/postgres";
-import { Suspense } from "react";
 
 export default async function Pets() {
   // استعلام لجلب جميع الحيوانات الأليفة من الجدول
   const { rows } = await sql`
     SELECT * FROM fonts
   `;
-  return (
-    <section>
-      <Suspense fallback={<p>Loading feed...</p>}>
-        {JSON.stringify(rows, null, 2)}
-      </Suspense>
-    </section>
-  );
+  return <section>{JSON.stringify(rows, null, 2)}</section>;
 }
