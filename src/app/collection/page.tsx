088f3f3284bcc52f97a48f2892fb5de557965a8b
mu7ammad1 <153460583+mu7ammad1@users.noreply.github.com>
2024-11-10 22:39:32 +0200

    1

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,4 +1,5 @@
-"use client"
+"use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import InsertLikes from "@/components/insertLikes";
 import Link from "next/link";
 import dayjs from "dayjs";
